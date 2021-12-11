856848280b6cfc4a30f5b75a4dc225dea8f2bcda
Nathan Garden <gardennathan07@gmail.com>
2021-12-11 22:53:49 +0000

    Almost fully done the revamp, small space character issue

--- death_toll.py
+++ death_toll.py
@@ -11,9 +11,22 @@ import keyboard
 file1 = 'data/deaths_and_times.json'
 file2 = 'data/number_deaths.json'
 # Basic files for this program
+
+
+first_msg = ("\nWelcome to Death counter!\n"
+    "\nThis program is ment to help keep track of deahts in games.\n"
+    "First you will be asked to type in the name of the game you are playing.\n"
+    "Then you will be prompted to type the boss you are fighting against.\n"
+    "After this the program is started\n"
+    "\nNow everytime you hit '/' a death will be added\n"
+    "Once you are done, press ] to either choose to:\n"
+    "\nRestart the program and add a new boss, or new boss and game.\n"
+    "\nClose the program.\n"
+    "Thank you for using this program!\n")
+# Msg to be displayed the first time the program is started
     
 def check_file():
-    """Checks if a file is empty, or dosnt exist"""
+    """Checks if a file is empty, or doesn't exist"""
     
     try:
         with open(file1, 'r'):
@@ -48,89 +61,76 @@ def make_or_load(empty):
     """Makes the dict and list or loads them"""
     
     game, boss = get_names()
-    
+
     if empty is True:
-        deaths = {game: {boss: {'deaths': {'times': {}}}}}
-        count = []
+        deaths = {game: {boss: {'times': {}}}}
+        count = False
+        return deaths, count, game, boss
         
-        
-        gbdp = True # gdpb means game, boss, death, brackets
-        gbdteb = False # means game, boss, death, times equals brackets
-        gbedb = False # means game, boss, equals deaths brackets
-        gebbdb = False # means game, equals boss, brackets, death, brackets
-        
-        
-        return deaths, count, gbdp, gbdteb, gbedb, gebbdb
-    
-    
     elif empty is False:
-        with open (file1, 'r' ) as f:
+        with open(file1, 'r') as f:
             deaths = json.load(f)
-        with open(file2, 'r') as f:
-            count = json.load(f)
         if game in deaths:
             if boss in deaths[game]:
-                deaths[game][boss]['deaths']['times'] = {}
-                count = [] # place holder
-                
-                
-                gbdp = False
-                gbdteb = True
-                gbedb = False
-                gebbdb = False
-                
-                
-                return deaths, count, gbdp, gbdteb, gbedb, gebbdb
-            
+                count = True
+                return deaths, count, game, boss
             
             elif boss not in deaths:
-                deaths[game][boss] = {'deaths': {'times': {}}}
-                count = [] # place holder 
-                
-                
-                gbdp = False
-                gbdteb = False
-                gbedb = True
-                gebbdb = False
-                
-                
-                return deaths, count, gbdp, gbdteb, gbedb, gebbdb
-            
+                deaths[game][boss] = {'times': {}}
+                count = False 
+                return deaths, count, game, boss
             
         elif game not in deaths:
-            with open(file1, 'w') as f:
-                deaths[game] = {boss: {'deaths': {'times': {}}}}
-                count = [] #place holder
-                
-                
-                gbdp = False
-                gbdteb = False
-                gbedb = False
-                gebbdb = True
-                
-                
-                return deaths, count, gbdp, gbdteb, gbedb, gebbdb
+            deaths[game] = {boss: {'times': {}}}
+            count = False
+            return deaths, count, game, boss
 # This is a bunch of true or false tests, to see what should happen with the data
-# aswell as to see what needs to happen on the counter function
-            
-            
+# as well as to see what needs to happen on the counter function
+
+    
 def main():
     """Main loop of the program."""
-    
-    while True:
-        empty = check_file()
-        deaths, count, o1, o2, o3, o4 = make_or_load(empty)
-        # o1, o2, o3, o4 are all basically a true or false test for
-        # the counter part
-        print(deaths, count)
-        print(f"\n{o1}:{o2}:{o3}:{o4}")
+
+    def counter():
+        number_deaths.append("1")
+        deaths[game][boss]['times'][str(len(number_deaths))] = {'time': time.strftime('%d: %m: %Y: %H_%M')}
+        
         with open(file1, 'w') as f:
             json.dump(deaths, f, indent=6)
-        with open(file2 , 'w') as f:
-            json.dump(count, f, indent=6)
+        with open(file2, 'w') as f:
+            json.dump(number_deaths, f, indent=6)
+
+    while True:
+        empty = check_file()
+        if empty is True:
+            print(first_msg)
+        elif empty is False:
+            print("Welcome back!\n")
+        deaths, count, game, boss = make_or_load(empty)
+
+        if count is True:
+            with open(file1, 'r') as f:
+                place = json.load(f)
+                number_deaths = []
+                for x in place[game][boss]['times'].keys():
+                    number_deaths.append("1")
+        
+        elif count is False:
+            number_deaths = []
+            
+        keyboard.add_hotkey("/", counter)
+        keyboard.wait("1")
         break
 # This is simply the main loop of the program
 # that gets all the data and sends it to the right place
 
-main()        
+
+while True:
+    main()
+    ans = input("Do you want to close or restart? (c/r): ").lower()
+    
+    if ans == "c":
+        break
+    elif ans == "r":
+        continue     
         
